b4a577528cc52a0d9d3a241178a35e97a73a4291
Liam Beeton <liam.beeton@gmail.com>
2018-05-23 10:01:32 +0200

    added new color scheme

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -11,39 +11,6 @@ $(document).ready(function () {
 		});
 	}
 
-	$("form").submit(function (e) {
-		e.preventDefault();
-		e.stopPropagation();
-
-		if ($(this)[0].checkValidity()) {
-			var name = $("#name").val();
-			var email = $("#email").val();
-			var telephone = $("#telephone").val();
-			var message = $("#message").val();
-
-			$.ajax({
-				type: "POST",
-				url: 'https://4zsc7rqq1a.execute-api.eu-west-1.amazonaws.com/prod',
-				contentType: 'application/json',
-				data: JSON.stringify({
-					'name': name,
-					'email': email,
-					'telephone': telephone,
-					'message': message
-				}),
-				success: function (res) {
-					$("form").trigger("reset");
-					$('#form-response').text('Email was sent.');
-				},
-				error: function () {
-					$('#form-response').text('Error.');
-				}
-			});
-		}
-
-		$(this).addClass('was-validated');
-	});
-
 	$(".navbar-nav .nav-link").on("click", function () {
 		$(".navbar-nav").find(".active").removeClass("active");
 		$(this).addClass("active");
